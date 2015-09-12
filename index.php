5cdbaf76c7016b4b9781b0f5c9455abfbdef5df9
Clemenshemmerling <clemens@galileo.edu>
2015-09-11 21:32:21 -0600

    cambio link
    cambio a la ruta de los estilos

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>MORINPLUS</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="http:///wordpress/wp-content/themes/MORINGA/style.css">
 
 </head>
 <body>
