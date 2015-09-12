f219433c57f9df39c44134380b262cd03cb0d46d
Clemenshemmerling <clemens@galileo.edu>
2015-09-11 21:42:09 -0600

    Correcion
    correccion de ip

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>MORINPLUS</title>
-	<link rel="stylesheet" type="text/css" href="http://localhost/wordpress/wp-content/themes/MORINGA/style.css">
+	<link rel="stylesheet" type="text/css" href="http://192.168.0.20/wordpress/wp-content/themes/MORINGA/style.css">
 
 </head>
 <body>
