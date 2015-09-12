129341d1ee28142a00e07968590f9c5f7eca7a2e
Clemenshemmerling <clemens@galileo.edu>
2015-09-12 13:36:02 -0600

    Se agrego estructura
    se le agrego la estructura de la pagina

--- index.php
+++ index.php
@@ -4,14 +4,43 @@
 	<meta charset="UTF-8">
 	<title>MorinPlus</title>
 	<link rel="stylesheet"  href="<?php bloginfo(stylesheet_url) ?>">
+	<link rel="stylesheet" type="text/css" href="style.css">
 
 </head>
 <body>
+	<div class"topbar">
+		<nav>
+		 <ul>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+		 </ul>
+	    </nav>
+	</div>   
 	<header class="header">
 		<h1 class="green-text"><?php bloginfo('name'); ?></h1>
 	    <h2 class="green-text"><?php bloginfo('description') ?> </h2>
 	</header>
-	<ul>
+	<section>
+		<header>
+			<h2>Beneficios</h2>
+		</header>
+		<article>
+			<header>
+				<h3>Sirve</h3>
+			</header>
+			<p>Acelera la pérdida de peso</p>
+
+		</article>
+		<article>
+		</article>
+		<article>
+		</article>
+		<article>
+		</article>
+	</section>
 		<li><?php bloginfo('name'); ?></li>
 		<li><?php bloginfo('description'); ?></li>
 		<li><?php bloginfo('wpurl'); ?></li>
