2b8f92a2639f1f95972ff152548aca042586c1b1
Clemenshemmerling <clemens@galileo.edu>
2015-09-12 15:05:19 -0600

    Dando estilos
    Le dimos estilos a los headers del section

--- index.php
+++ index.php
@@ -24,22 +24,44 @@
 	    <h2 class="green-text"><?php bloginfo('description') ?> </h2>
 	</header>
 	<section>
-		<header>
+		<header class="header-section">
 			<h2>Beneficios</h2>
 		</header>
 		<article>
 			<header>
-				<h3>Sirve</h3>
+				<h3 class="grey-text">Sirve</h3>
+				<h4 class="green-text">como un potenciador sexual</h4>
 			</header>
-			<p>Acelera la pérdida de peso</p>
+			<p class="green-text">
+			Una sopa hecha con cerca de quince gramos de hojas de moringa hervidas en 250 ml de leche es muy útil como tónico sexual en el tratamiento de la debilidad sexual. Es también útil en esterilidad funcional, tanto en hombres como en mujeres. El polvo de la corteza seca tiene también valor en impotencia, eyaculación precoz y la delgadez de semen. Cerca de 120 gm del polvo de la corteza seca se deben hervir en medio litro de agua durante media hora. Treinta gramos de este polvo, mezclado con una cucharada de miel, se debe tomar tres veces al día durante un mes. Las pastillas de Moringa se pueden tomar 1 tres veces al dia por 30 dias para mejores resultados.
+		    </p>
 
 		</article>
 		<article>
+			<header>
+				<h3 class="grey-text">Sirve</h3>
+				<h4 class="green-text">como un potenciador sexual</h4>
+			</header>
+			<p class="green-text">Acelera la pérdida de peso</p>
+
 		</article>
 		<article>
+			<header>
+				<h3 class="grey-text">Reduce</h3>
+				<h4 class="green-text">la desnutrición</h4>
+			</header>
+			<p class="green-text">La falta de consumo balanceado de nutrientes o alimentos erróneos, lleva a una acumulación de toxinas dentro del cuerpo, dando como resultado enfermedades crónicas a largo plazo. Ya que un solo grupo de alimentos no puede nutrir al cuerpo con todos los ingredientes vitales que se requieren. La Moringa proporciona a los niños las vitaminas necesarias para mantener su organismo en óptimas condiciones.</p>
+
 		</article>
 		<article>
+			<header>
+				<h3 class="grey-text">Proporciona</h3>
+				<h4 class="green-text">vitaminas para el crecimiento de los niños</h4>
+			</header>
+			<p class="green-text">La Moringa ayuda a los cuerpos en crecimiento rápido, ya que necesitan de nutrientes para ayudar en el crecimiento muscular y óseo, el desarrollo del cerebro, la claridad mental, la salud de la piel, cabello y uñas, y sobre todo para fortalecer el sistema inmunológico.</p>
+
 		</article>
+		
 	</section>
 		<li><?php bloginfo('name'); ?></li>
 		<li><?php bloginfo('description'); ?></li>
